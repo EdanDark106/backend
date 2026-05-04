c26739176f6d22c13b7d9c78b43fcd690d47e736
Erwin106 <142272630+EdanDark106@users.noreply.github.com>
2026-05-04 13:30:54 -0400

    Add files via upload

--- up/uppart2/index.js
+++ up/uppart2/index.js
@@ -11,10 +11,36 @@ const pool = mysql.createPool({
     database: 'basededatos'
 });
 
-app.get('/', async (req, res) => {
-    const [resultado] = await pool.query('SELECT * FROM productos');
-    res.send(resultado);
+app.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const [resultado] = await pool.query('SELECT * FROM productos WHERE id = ?', [id]);
+    res.send(resultado[0]);
 });
+
+app.post('/', async(req, res) => {
+    const{nombre,precio} = req.body;
+    const [resultado] = await pool.query(
+        'INSERT INTO productos(nombre,precio)objectToValues(?,?)',
+        [nombre, precio]); 
+    res.send(resultado);
+})
+
+app.patch('/:id', async(req, res) => {
+    const id = req.params.id;
+    const{nombre,precio} = req.body;
+    const [resultado] = await pool.query(
+        'UPDATE productos set nombre = ?, precio = ? WHERE id = ?',
+        [nombre, precio,id]); 
+    res.send(resultado);
+})
+
+app.delete('/:id', async(req, res) => {
+    const id = req.params.id;
+    const [resultado] = await pool.query(
+        'DELETE FROM productos WHERE id = ?',
+        [id]); 
+    res.send({mensaje:'producto eliminado correctamente' });
+})
 
 const PUERTO = 3001;
 app.listen(PUERTO, () => {
---
+++ up/uppart2/prueba.js
@@ -0,0 +1,11 @@
+let a = [[1,2,3].[4,5,6,7]]
+let [primero] = a
+let b = a
+console.log(b);
+console.log(primero);
+console.log(segundo);
+
+
+//node proy777
+//cls
+//
